31099a96b84b502554e958d880360f89fc4240ad
plinkyplunky <60498045+plinkyplunky@users.noreply.github.com>
2020-01-31 18:16:08 +0800

    double型最大最小正数值(没有特别精确)

---
+++ x1-3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <limits>
+using namespace std;
+int main()
+{
+    cout << numeric_limits<double>::max() << endl;
+    cout << numeric_limits<double>::min() << endl;
+    system("pause");
+    return 0;
+}
