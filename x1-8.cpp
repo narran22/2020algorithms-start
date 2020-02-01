51ed77d2f5aa806b11da68f381dfc2f556c534bc
plinkyplunky <60498045+plinkyplunky@users.noreply.github.com>
2020-02-01 15:54:56 +0800

    文件输入输出--数据统计
    输出数字个数 输出最大值 最小值 平均值 以0为结束标志

--- x1-8.cpp
+++ x1-8.cpp
@@ -2,24 +2,30 @@
 #define INF 1000000000
 int main()
 {
+    //文件输入输出
     FILE *fin, *fout;
     fin = fopen("data.in", "rb");
     fout = fopen("data.out", "wb");
-    int x;
     int n = 0;
-    int MAX = -INF;
-    int MIN = INF;
-    double s = 0;
-    while (fscanf(fin, "%d", &x) == 1)
+    int kase = 0;
+    while (fscanf(fin, "%d", &n) && n) //当n=0的时候 结束输出
     {
-        s += x;
-        if (x < MIN)
-            MIN = x;
-        if (x > MAX)
-            MAX = x;
-        n++;
+        kase++;
+        int MAX = -INF;
+        int MIN = INF;
+        double s = 0;
+        int tmp;
+        for (int i = 0; i < n; i++)
+        {
+            fscanf(fin, "%d", &tmp);
+            if (tmp < MIN)
+                MIN = tmp;
+            if (tmp > MAX)
+                MAX = tmp;
+            s += tmp;
+        }
+        fprintf(fout, "Case %d：%d %d %.3f\n", kase, MIN, MAX, (double)s / n);
     }
-    fprintf(fout, "%d %d %.3f\n", MIN, MAX, (double)s / n);
     fclose(fin);
     fclose(fout);
     return 0;
